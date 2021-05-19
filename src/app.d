dba944d8d4405185099d61ab426aecdd0bfa785b
kookman <kookman@users.noreply.github.com>
2021-05-19 22:00:32 +1000

    Updated example/test app and README.

--- src/app.d
+++ src/app.d
@@ -4,6 +4,9 @@ import core.time;
 
 import nats.client;
 
+// minimise overhead by turning off parallel GC. We don't generate much garbage anyway
+extern(C) __gshared string[] rt_options = ["gcopt=parallel:0"];
+
 Nats natsConn;
 
 void main(string[] args)
@@ -20,20 +23,26 @@ void main(string[] args)
                 natsConn.subscribe(">", toDelegate(&sub_all_handler));
                 logInfo("Sent subscribe all.");
                 auto greets = natsConn.subscribe("greetings", toDelegate(&greetings_handler));
-                auto responder = natsConn.subscribe("MoL", toDelegate(&responder));
+                auto responder = natsConn.subscribe("query", toDelegate(&query_responder));
+                auto shutdown = natsConn.subscribe("exit", toDelegate(&shutdown_handler));
                 natsConn.publish("back_channel", cast(ubyte[])"This came on the back channel...");
                 natsConn.publish("greetings", cast(ubyte[])"Hello to all greetings subscribers!");		
                 sleep(50.msecs);
                 natsConn.unsubscribe(greets);
-                natsConn.publishRequest("MoL", cast(ubyte[])"What is the meaning of life?", toDelegate(&response_handler));
+                natsConn.publishRequest("query", cast(ubyte[])"721256", toDelegate(&response_handler));
                 sleep(5000.msecs);
                 natsConn.publish("greetings", cast(ubyte[])"Still there?");
-                natsConn.publishRequest("MoL", cast(ubyte[])"Are you sure its 42?", toDelegate(&response_handler));
+                natsConn.publishRequest("query", cast(ubyte[])"722739", toDelegate(&response_handler));
             });
 
     runApplication();
 }
 
+void shutdown_handler(scope Msg msg) @safe
+{
+    logInfo("Shutdown msg received. Shutting down.");
+    exitEventLoop();
+}
 
 void sub_all_handler(scope Msg msg) @safe
 {
@@ -45,16 +54,27 @@ void greetings_handler(scope Msg msg) @safe
     logInfo("greetings_handler--> %s", msg.payloadAsString);
 }
 
-void responder(scope Msg msg) @safe
+void query_responder(scope Msg msg) @safe
 {
-    import std.string: representation;
-    import std.exception: assumeUnique;
+    import std.conv: to;
+    import std.format: sformat;
+    import std.random: uniform;
 
-    logInfo("responder--> starting long calc...");  
-    sleep(1.seconds);   // note: this currently blocks the listener task
-    logInfo("responder--> done. Sending reponse to %s", msg.replySubject);
-    auto replySubject = () @trusted { return msg.replySubject.assumeUnique; }();
-    natsConn.publish(replySubject, "The answer is 42!".representation);
+    void responder(string reply, int data, Nats conn)
+    {
+        char[80] buffer;
+        int delay = uniform(0, 1000);
+        logInfo("responder %d --> starting %d ms long calc...", data, delay);  
+        sleep(delay.msecs); 
+        logInfo("responder %d --> done. Sending reponse to %s", data, reply);
+        conn.publish(reply, buffer.sformat!"Answering query number %d"(data));
+    }
+
+    // note: we runTask to avoid blocking the listener task
+    // so we must extract the msg contents we need (since it is scoped)
+    auto replySubject = msg.replySubject.idup;
+    int id = to!int(msg.payloadAsString);
+    runTask(&responder, replySubject, id, natsConn);
 }
 
 void response_handler(scope Msg msg) @safe
